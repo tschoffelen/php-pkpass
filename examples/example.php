22f9fb4d372843c86f8fc23edfde0879f154b902
Mike Almond <almond@raceroster.com>
2016-04-06 14:25:16 -0400

    Clean up example files

--- examples/example.php
+++ examples/example.php
@@ -1,69 +1,71 @@
 <?php
-require('src/PKPass.php');
+use PKPass\PKPass;
 
-$pass = new PKPass\PKPass();
+require('../PKPass.php');
 
-$pass->setCertificate('../Certificate.p12');  // 2. Set the path to your Pass Certificate (.p12 file)
+$pass = new PKPass();
+
+$pass->setCertificate('../Certificate.p12');  // 1. Set the path to your Pass Certificate (.p12 file)
 $pass->setCertificatePassword('test123');     // 2. Set password for certificate
 $pass->setWWDRcertPath('../AppleWWDRCA.pem'); // 3. Set the path to your WWDR Intermediate certificate (.pem file)
 
 // Top-Level Keys http://developer.apple.com/library/ios/#documentation/userexperience/Reference/PassKit_Bundle/Chapters/TopLevel.html
-$standardKeys         = array(
+$standardKeys         = [
     'description'        => 'Demo pass',
     'formatVersion'      => 1,
     'organizationName'   => 'Flight Express',
     'passTypeIdentifier' => 'pass.com.apple.test', // 4. Set to yours
     'serialNumber'       => '123456',
     'teamIdentifier'     => 'AGK5BZEN3E'           // 4. Set to yours
-);
-$associatedAppKeys    = array();
-$relevanceKeys        = array();
-$styleKeys            = array(
-    'boardingPass' => array(
-        'primaryFields' => array(
-            array(
+];
+$associatedAppKeys    = [];
+$relevanceKeys        = [];
+$styleKeys            = [
+    'boardingPass' => [
+        'primaryFields'   => [
+            [
                 'key'   => 'origin',
                 'label' => 'San Francisco',
-                'value' => 'SFO'
-            ),
-            array(
+                'value' => 'SFO',
+            ],
+            [
                 'key'   => 'destination',
                 'label' => 'London',
-                'value' => 'LHR'
-            )
-        ),
-        'secondaryFields' => array(
-            array(
+                'value' => 'LHR',
+            ],
+        ],
+        'secondaryFields' => [
+            [
                 'key'   => 'gate',
                 'label' => 'Gate',
-                'value' => 'F12'
-            ),
-            array(
+                'value' => 'F12',
+            ],
+            [
                 'key'   => 'date',
                 'label' => 'Departure date',
-                'value' => '07/11/2012 10:22'
-            )
-        ),
-        'backFields' => array(
-            array(
+                'value' => '07/11/2012 10:22',
+            ],
+        ],
+        'backFields'      => [
+            [
                 'key'   => 'passenger-name',
                 'label' => 'Passenger',
-                'value' => 'John Appleseed'
-            )
-        ),
-        'transitType' => 'PKTransitTypeAir'
-    )
-);
-$visualAppearanceKeys = array(
-    'barcode'         => array(
+                'value' => 'John Appleseed',
+            ],
+        ],
+        'transitType'     => 'PKTransitTypeAir',
+    ],
+];
+$visualAppearanceKeys = [
+    'barcode'         => [
         'format'          => 'PKBarcodeFormatQR',
         'message'         => 'Flight-GateF12-ID6643679AH7B',
-        'messageEncoding' => 'iso-8859-1'
-    ),
+        'messageEncoding' => 'iso-8859-1',
+    ],
     'backgroundColor' => 'rgb(107,156,196)',
-    'logoText'        => 'Flight info'
-);
-$webServiceKeys       = array();
+    'logoText'        => 'Flight info',
+];
+$webServiceKeys       = [];
 
 // Merge all pass data and set JSON for $pass object
 $passData = array_merge(
@@ -82,6 +84,6 @@ $pass->addFile('images/icon.png');
 $pass->addFile('images/icon@2x.png');
 $pass->addFile('images/logo.png');
 
-if(!$pass->create(true)) { // Create and output the PKPass
-    echo 'Error: '.$pass->getError();
+if ( !$pass->create(true)) { // Create and output the PKPass
+    echo 'Error: ' . $pass->getError();
 }
