723ec04e703a37af48ca55801b7b64883398ac6c
Anders Knutsson <aknutsson@outlook.com>
2020-07-21 21:19:37 +0200

    Revert "Encode LocaleStrings before adding"
    This reverts commit f8aaf24704fa486794fc63bbbc163e999e3b19e7.

--- src/PKPass.php
+++ src/PKPass.php
@@ -237,7 +237,7 @@ class PKPass
         }
         $dictionary = "";
         foreach($strings as $key => $value) {
-            $dictionary .= '"'. $this->escapeLocaleString($key) .'" = "'. $this->escapeLocaleString($value) .'";'. PHP_EOL;
+            $dictionary .= '"'. $key .'" = "'. $value .'";'. PHP_EOL;
         }
         $this->locales[$language] = $dictionary;
 
@@ -664,18 +664,5 @@ class PKPass
         }
 
         return true;
-    }
-
-    protected static $escapeChars = [
-        "\n" => "\\n",
-        "\r" => "\\r",
-        "\"" => "\\\"",
-        "\\" => "\\\\"
-    ];
-    /**
-     * Escapes strings for use in locale files
-     */
-    protected function escapeLocaleString($string) {
-        return strtr($string, self::$escapeChars);
     }
 }
